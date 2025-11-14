5a039a5ba406a868a661e9ce19c78cc5dda8c267
d o <@>
2025-11-14 12:27:25 +0100

    add some asset defs for testing

--- author/assetDefinitions.js
+++ author/assetDefinitions.js
@@ -1,6 +1,12 @@
 
 var $_assetDefinitions = `
 
+smile1: puretestassets/eve/smile1.jpeg
+smile2: puretestassets/eve/smile2.jpeg
+
+coins: puretestassets/audio/coins.wav
+rain: puretestassets/audio/rain.ogg
+slam: puretestassets/audio/slam.mp3
 
 
 `
--- js/app.js
+++ js/app.js
@@ -93,6 +93,8 @@
 
     restartStoryInitialState = getState()
 
+    refreshUndoIcon()
+
   }
 
 
