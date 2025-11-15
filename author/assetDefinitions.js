bd9168c313e76adc02b21944586ccbe4abee8142
d o <@>
2025-11-15 15:07:27 +0100

    demo story

--- author/assetDefinitions.js
+++ author/assetDefinitions.js
@@ -1,12 +1,7 @@
 
 var $_assetDefinitions = `
 
-smile1: puretestassets/eve/smile1.jpeg
-smile2: puretestassets/eve/smile2.jpeg
-
-coins: puretestassets/audio/coins.wav
-rain: puretestassets/audio/rain.ogg
-slam: puretestassets/audio/slam.mp3
+rain1: example-game/rain-blur-by-pexels.jpg
 
 
 `
--- story.ink
+++ story.ink
@@ -1,4 +1,7 @@
 
 Welcome to Inchiostro! :)
 
+$ image rain1
+$ image: name = rain1
+
 -> DONE
